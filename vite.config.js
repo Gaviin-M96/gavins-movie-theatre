d2080abd920d4d9ec7f1fcd080330d94836e3257
Gavin Moore <gavinmoore.cs@gmail.com>
2025-11-23 17:07:08 -0500

    Fix Vite base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // base: "/gavins-movie-theatre/",  // ❌ remove this line
+  base: "/",                          // or set explicitly to "/"
   plugins: [react()],
-  base: "/gavins-movie-theatre/", //
 });
